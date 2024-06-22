9ed22129395eeed29b927f26af056632625a724d
chen <toaff100@gmail.com>
2024-06-22 17:53:41 +0300

    refactor: remove files due to folder structure reorganization

--- server/src/models/element.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const elementSchema = mongoose.Schema({
-  type: String,
-  content: String,
-  noteId: { type: mongoose.Types.ObjectId, ref: "notes" },
-});
-
-module.exports = mongoose.model("elements", elementSchema);
--- server/src/routes/element/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-router.delete("/:id", require("./delete"));
-router.post("/", require("./create"));
-// user.get("/:id", require("./get"));
-router.patch("/:id", require("./edit"));
-
-module.exports = router;
