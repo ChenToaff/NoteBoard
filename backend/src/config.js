5a7e21f0b82774ac33f5b06a02960668d1cb6536
chen <toaff100@gmail.com>
2024-06-24 19:19:26 +0300

    feat(backend): add login capability

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME || "admin",
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "admin",
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1/Notes",
   JWT_SECRET:
--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,14 +1,20 @@
-const login = async (req, res) => {
-  let { userName, password } = req.body;
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, ADMIN_PASSWORD, ADMIN_USERNAME } = require("../config");
 
-  if (!userName || !password) {
+const login = async (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
     return res.status(400).send("missing values");
   }
-  const access_token = jwt.sign({ role: "admin" }, JWT_SECRET, {
+  if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+    return res.status(404).send("Wrong username or password");
+  }
+  const accessToken = jwt.sign({ role: "admin" }, JWT_SECRET, {
     expiresIn: "1h",
   });
-
-  return res.status(200).send(access_token);
+  res.cookie("token", accessToken, { maxAge: 900000, httpOnly: true });
+  return res.status(200);
 };
 
 module.exports = { login };
