4109767482abff1f933f92d5a42ceface080f3d6
chen <toaff100@gmail.com>
2024-06-25 20:37:49 +0300

    refactor(backend): implement usage of ApiError in authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebtoken");
+const { ApiError } = require("../utils/ApiError");
 const { JWT_SECRET, ADMIN_PASSWORD, ADMIN_USERNAME } = require("../config");
 
-const login = async (req, res) => {
+const login = (req, res) => {
   let { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).send("missing values");
+    throw new ApiError(400, "missing values");
   }
   if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
-    return res.status(404).send("Wrong username or password");
+    throw new ApiError(400, "Wrong username or password");
   }
   const accessToken = jwt.sign({ role: "admin" }, JWT_SECRET, {
     expiresIn: "1h",
