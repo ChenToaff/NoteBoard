dd77a286625f939528d36a04749afd1399f430a6
chen <toaff100@gmail.com>
2024-06-28 22:25:35 +0300

    feat(backend): add id property to note

--- backend/src/models/NoteModel.js
+++ backend/src/models/NoteModel.js
@@ -15,5 +15,12 @@ const NoteSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+NoteSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+// Ensure virtuals are included when converting a document to JSON/objects
+NoteSchema.set("toJSON", { virtuals: true });
+NoteSchema.set("toObject", { virtuals: true });
 
 module.exports = mongoose.model("notes", NoteSchema);
